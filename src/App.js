8a86f55da7bdbf7ae41500118a38451803b58f57
Joe <modulusman@gmail.com>
2020-07-02 23:28:53 -0700

    Adds search bar component and functionality.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import './App.css'
 import Header from './components/Header'
 import axios from 'axios'
 import CharacterGrid from './components/CharacterGrid'
+import Search from './components/Search'
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
-      console.log(result.data);
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
       setItems(result.data);
       setIsLoading(false);
     }
 
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
