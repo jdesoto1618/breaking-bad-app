b395252357b8dde7a6240bd009178bf1c49fa070
Joe <modulusman@gmail.com>
2020-07-02 22:28:43 -0700

    Adds header component and places this in App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/Header.js'
 
 const App = () => {
   return (
     <div className="container">
-      Hello World!
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../images/main-logo.png'
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logo} alt='logo.png' />
+    </header>
+  )
+}
+
+export default Header
